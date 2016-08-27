a0bf58d42e0ffb21ce37c8a0bd1547871b84395b
LRGN <lrgn@protonmail.com>
2016-08-27 13:03:02 +0900

    Add base exception class

---
+++ heavencore-common/src/main/java/fr/hc/core/exceptions/HeavenException.java
@@ -0,0 +1,16 @@
+package fr.hc.core.exceptions;
+
+public class HeavenException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+
+	public HeavenException(String message)
+	{
+		super(message);
+	}
+
+	public HeavenException(String message, Object... args)
+	{
+		super(String.format(message, args));
+	}
+}
