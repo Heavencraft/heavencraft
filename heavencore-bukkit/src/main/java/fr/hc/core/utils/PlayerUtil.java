14efca42cfd97ab127aa3833356e9a2de9c73c95
lorgan17 <lorgan17@gmail.com>
2016-09-14 23:59:47 +0200

    PlayerUtil implemented in Core instead of RP. Corrections made

--- heavencore-bukkit/src/main/java/fr/hc/core/utils/PlayerUtil.java
+++ heavencore-bukkit/src/main/java/fr/hc/core/utils/PlayerUtil.java
@@ -1,19 +1,16 @@
-package fr.hc.rp.player;
+package fr.hc.core.utils;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 import fr.hc.core.exceptions.PlayerNotConnectedException;
-import fr.hc.core.utils.chat.ChatUtil;
 
-public class PlayerUtil 
+public class PlayerUtil
 {
 	public static Player getPlayer(String playerName) throws PlayerNotConnectedException
 	{
-		@SuppressWarnings("deprecation")
 		final Player player = Bukkit.getPlayer(playerName);
 
 		if (player == null)
@@ -51,41 +48,7 @@ public class PlayerUtil
 	{
 		player.teleport(location);
 	}
-//
-//	public static void teleportPlayer(Player player, Entity entity)
-//	{
-//		ActionsHandler.addAction(new TeleportPlayerAction(player, entity));
-//	}
-//
-//	public static void teleportPlayer(Player player, Location location, String message, Object... args)
-//	{
-//		ActionsHandler.addAction(new TeleportPlayerAction(player, location)
-//		{
-//			@Override
-//			public void onSuccess()
-//			{
-//				ChatUtil.sendMessage(player, message, args);
-//			}
-//		});
-//	}
 
-//	public static void teleportPlayer(Player player, Entity entity, String message, Object... args)
-//	{
-//		ActionsHandler.addAction(new TeleportPlayerAction(player, entity)
-//		{
-//			@Override
-//			public void onSuccess()
-//			{
-//				ChatUtil.sendMessage(player, message, args);
-//			}
-//		});
-//	}
-	
-	/**
-	 * Returns the UUID of the player wihout "-"
-	 * @param player
-	 * @return
-	 */
 	public static String getUUID(Player player)
 	{
 		return player.getUniqueId().toString().replaceAll("-", "");
