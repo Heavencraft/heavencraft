132c6954cce68b4c8cdbe1488a9b3383c95a5059
Manuel R <rauscher.manuel@gmail.com>
2016-09-15 01:38:00 +0200

    Advenced message API Added util to create advenced chat messages using JSON format and tellraw hack. Allows creation of fancy messages with tooltips.

--- heavencore-bukkit/src/main/java/fr/hc/core/utils/chat/ChatUtil.java
+++ heavencore-bukkit/src/main/java/fr/hc/core/utils/chat/ChatUtil.java
@@ -1,11 +1,20 @@
 package fr.hc.core.utils.chat;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import fr.hc.core.chat.ChatMessage;
 
 public class ChatUtil
 {
 	public static void sendMessage(CommandSender sender, String message, Object... args)
 	{
 		new SendMessageTask(sender, message, args).schedule();
+	}
+	
+	public static void sendMessage(Player reciever, ChatMessage msg)
+	{
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tellraw " + reciever.getName() + " " + msg.build());
 	}
 }
