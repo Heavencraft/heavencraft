13834166150af24ba7a214240ea86acf3eba78a0
Mythridor <enoubia@gmail.com>
2016-09-15 00:00:21 +0200

    ModoCommands added

---
+++ heavencore-bukkit/src/main/java/fr/hc/core/commands/InventoryCommand.java
@@ -0,0 +1,45 @@
+package fr.hc.core.commands;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import fr.hc.core.AbstractBukkitPlugin;
+import fr.hc.core.AbstractCommandExecutor;
+import fr.hc.core.exceptions.HeavenException;
+import fr.hc.core.utils.PlayerUtil;
+import fr.hc.core.utils.chat.ChatUtil;
+
+public class InventoryCommand extends AbstractCommandExecutor
+{
+
+	public InventoryCommand(AbstractBukkitPlugin plugin)
+	{
+		super(plugin, "inventory");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onPlayerCommand(Player player, String[] args) throws HeavenException
+	{
+		if (args.length != 1)
+		{
+			sendUsage(player);
+			return;
+		}
+
+		player.openInventory(PlayerUtil.getPlayer(args[0]).getInventory());
+	}
+
+	@Override
+	protected void onConsoleCommand(CommandSender sender, String[] args) throws HeavenException
+	{
+		ChatUtil.sendMessage(sender, "Cette commande ne peut pas être utilisée depuis la {console}.");
+	}
+
+	@Override
+	protected void sendUsage(CommandSender sender)
+	{
+		ChatUtil.sendMessage(sender, "{/inventory} <joueur>");
+	}
+
+}
