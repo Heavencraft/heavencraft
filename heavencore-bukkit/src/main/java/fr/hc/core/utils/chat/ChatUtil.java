8bcead6f599e44edbc3f0ddd777187f8f0230f13
Manuel R <rauscher.manuel@gmail.com>
2016-09-17 00:37:25 +0200

    Removed Chat API Because Spigot already has one... -_-

--- heavencore-bukkit/src/main/java/fr/hc/core/utils/chat/ChatUtil.java
+++ heavencore-bukkit/src/main/java/fr/hc/core/utils/chat/ChatUtil.java
@@ -1,10 +1,6 @@
 package fr.hc.core.utils.chat;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
-import fr.hc.core.chat.ChatMessage;
 
 public class ChatUtil
 {
@@ -13,8 +9,4 @@ public class ChatUtil
 		new SendMessageTask(sender, message, args).schedule();
 	}
 	
-	public static void sendMessage(Player reciever, ChatMessage msg)
-	{
-		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tellraw " + reciever.getName() + " " + msg.build());
-	}
 }
