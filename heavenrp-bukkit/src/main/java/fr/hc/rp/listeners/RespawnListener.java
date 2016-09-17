b0a9adb93963bd2ad6d31197361f15b5d1b66df5
Mythridor <enoubia@gmail.com>
2016-09-18 01:46:43 +0200

    Spawn&Respawn listeners implemented

---
+++ heavenrp-bukkit/src/main/java/fr/hc/rp/listeners/RespawnListener.java
@@ -0,0 +1,25 @@
+package fr.hc.rp.listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+import fr.hc.core.AbstractBukkitListener;
+import fr.hc.core.AbstractBukkitPlugin;
+
+public class RespawnListener extends AbstractBukkitListener
+{
+	public RespawnListener(AbstractBukkitPlugin plugin)
+	{
+		super(plugin);
+	}
+
+	public void onRespawn(PlayerRespawnEvent e)
+	{
+		final Player player = e.getPlayer();
+		final Location spawn = new Location(Bukkit.getWorld("world"), 351, 83, 1041, 90, 0);
+
+		player.teleport(spawn);
+	}
+}
