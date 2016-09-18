0a796075eb3bced0ffe7793523811466a01dd50d
Manuel R <rauscher.manuel@gmail.com>
2016-09-18 18:14:33 +0200

    Fixed wrong permission

--- heavencore-common/src/main/java/fr/hc/core/CorePermissions.java
+++ heavencore-common/src/main/java/fr/hc/core/CorePermissions.java
@@ -4,7 +4,7 @@ public class CorePermissions
 {
 	public static final String CREACHEAT_COMMAND = "heavencore.commands.creacheat";
 	public static final String ENDERCHEAT_COMMAND = "heavencore.commands.endercheat";
-	public static final String HEAL_COMMAND = "heavencore.commands.endercheat";
+	public static final String HEAL_COMMAND = "heavencore.commands.heal";
 	public static final String INVENTORY_COMMAND = "heavencore.commands.inventory";
 	public static final String ROUCOUPS_COMMAND = "heavencore.commands.roucoups";
 	public static final String SPECTATOR_COMMAND = "heavencore.commands.spectator";
