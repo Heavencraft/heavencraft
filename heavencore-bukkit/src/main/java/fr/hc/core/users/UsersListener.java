3a8a4945bab1d2f7e8452a2098e110d388f8b646
LRGN <lrgn@protonmail.com>
2016-09-27 23:32:10 +0900

    HeavenRP: Add economy base system (wining and losing money)

--- heavencore-bukkit/src/main/java/fr/hc/core/users/UsersListener.java
+++ heavencore-bukkit/src/main/java/fr/hc/core/users/UsersListener.java
@@ -3,6 +3,7 @@ package fr.hc.core.users;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -14,7 +15,9 @@ import fr.hc.core.db.users.UpdateUserLastLoginQuery;
 import fr.hc.core.db.users.UpdateUserNameQuery;
 import fr.hc.core.db.users.User;
 import fr.hc.core.db.users.UserProvider;
+import fr.hc.core.event.FirstLoginEvent;
 import fr.hc.core.exceptions.HeavenException;
+import fr.hc.core.utils.DateUtil;
 
 public class UsersListener extends AbstractBukkitListener
 {
@@ -50,7 +53,13 @@ public class UsersListener extends AbstractBukkitListener
 				new UpdateUserNameQuery(user, name, userProvider).schedule();
 			}
 
-			new UpdateUserLastLoginQuery(user, userProvider);
+			if (!DateUtil.isToday(user.getLastLogin()))
+			{
+				log.info("First connection of {}, sending FirstLoginEvent", user);
+				Bukkit.getPluginManager().callEvent(new FirstLoginEvent(player, user));
+			}
+
+			new UpdateUserLastLoginQuery(user, userProvider).schedule();
 		}
 		catch (final HeavenException ex)
 		{
