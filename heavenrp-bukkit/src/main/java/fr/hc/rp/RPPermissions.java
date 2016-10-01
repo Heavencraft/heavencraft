25259c5751fd780a2010a3f0107c6f611dc4855d
Mythridor <enoubia@gmail.com>
2016-10-01 04:22:41 +0200

    Warp command added in RPPermissions

--- heavenrp-bukkit/src/main/java/fr/hc/rp/RPPermissions.java
+++ heavenrp-bukkit/src/main/java/fr/hc/rp/RPPermissions.java
@@ -4,4 +4,5 @@ public class RPPermissions
 {
 	public static final String ADD_MAYOR = "heavenrp.town.mayor.add";
 	public static final String REMOVE_MAYOR = "heavenrp.town.mayor.remove";
+	public static final String WARP_COMMAND = "heavenrp.command.warp";
 }
