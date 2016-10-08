04e8285dc9c57e99bcc13efd0673e6606d6aa81b
lorgan17 <lorgan17@gmail.com>
2016-10-08 10:47:15 +0900

    HeavenRP/Shops: add possibility to get stocks by location

--- heavenrp-common/src/main/java/fr/hc/rp/db/stocks/StockCache.java
+++ heavenrp-common/src/main/java/fr/hc/rp/db/stocks/StockCache.java
@@ -6,6 +6,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import fr.hc.core.HeavenBlockLocation;
+
 // Available from package only
 class StockCache
 {
@@ -13,6 +15,7 @@ class StockCache
 
 	private final Map<Integer, Stock> stocksById = new ConcurrentHashMap<Integer, Stock>();
 	private final Map<CompanyIdAndStockName, Stock> stocksByNameByCompanyId = new ConcurrentHashMap<CompanyIdAndStockName, Stock>();
+	private final Map<HeavenBlockLocation, Stock> stocksByLocation = new ConcurrentHashMap<HeavenBlockLocation, Stock>();
 
 	public Stock getStockById(int id)
 	{
@@ -24,16 +27,23 @@ class StockCache
 		return stocksByNameByCompanyId.get(companyIdAndStockName);
 	}
 
+	public Stock getStockByLocation(HeavenBlockLocation location)
+	{
+		return stocksByLocation.get(location);
+	}
+
 	public void addToCache(Stock stock)
 	{
 		stocksById.put(stock.getId(), stock);
 		stocksByNameByCompanyId.put(stock.getCompanyIdAndStockName(), stock);
+		stocksByLocation.put(stock.getLocation(), stock);
 	}
 
 	public void invalidateCache(Stock stock)
 	{
 		stocksById.remove(stock.getId());
 		stocksByNameByCompanyId.remove(stock.getCompanyIdAndStockName());
+		stocksByLocation.remove(stock.getLocation());
 
 		log.info("Invalidated stock cache for {}", stock);
 	}
