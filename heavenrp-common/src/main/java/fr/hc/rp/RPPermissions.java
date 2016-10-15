e69666106fba78546c43b75a7f307d139ef519f8
LRGN <lrgn@protonmail.com>
2016-10-15 11:26:03 +0900

    HeavenRP/Shops: allow player to create companies by themselves

--- heavenrp-common/src/main/java/fr/hc/rp/RPPermissions.java
+++ heavenrp-common/src/main/java/fr/hc/rp/RPPermissions.java
@@ -6,7 +6,6 @@ public class RPPermissions
 	public static final String REMOVE_MAYOR = "heavenrp.town.mayor.remove";
 	public static final String WARP_COMMAND = "heavenrp.command.warp";
 
-	public static final String ENTREPRISE_CREER_COMMAND = "heavenrp.commands.entreprise.creer";
 	public static final String ENTREPRISE_AJOUTEREMPLOYEUR_COMMAND = "heavenrp.commands.entreprise.ajouteremployeur";
 	public static final String ENTREPRISE_AJOUTEREMPLOYE_COMMAND = "heavenrp.commands.entreprise.ajouteremploye";
 	public static final String ENTREPRISE_RETIREREMPLOYEUR_COMMAND = "heavenrp.commands.entreprise.retireremployeur";
