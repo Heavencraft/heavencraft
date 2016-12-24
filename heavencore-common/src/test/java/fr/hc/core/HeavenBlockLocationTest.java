23666849ca671494295a808e7ddc21453e04b0cb
lorgan17 <lorgan17@gmail.com>
2016-12-24 10:15:16 +0900

    Core: improve unit tests

--- heavencore-common/src/test/java/fr/hc/core/HeavenBlockLocationTest.java
+++ heavencore-common/src/test/java/fr/hc/core/HeavenBlockLocationTest.java
@@ -1,5 +1,9 @@
 package fr.hc.core;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
 import junit.framework.TestCase;
 
 public class HeavenBlockLocationTest extends TestCase
@@ -8,21 +12,28 @@ public class HeavenBlockLocationTest extends TestCase
 	{
 		final HeavenBlockLocation location = new HeavenBlockLocation("world", 1, 2, 3);
 
-		assertEquals(true, location.equals(new HeavenBlockLocation("world", 1, 2, 3)));
-		assertEquals(false, location.equals(new HeavenBlockLocation("world_nether", 1, 2, 3)));
-		assertEquals(false, location.equals(new HeavenBlockLocation("world", 10, 2, 3)));
-		assertEquals(false, location.equals(new HeavenBlockLocation("world", 1, 20, 3)));
-		assertEquals(false, location.equals(new HeavenBlockLocation("world", 1, 2, 30)));
+		assertThat(location, equalTo(new HeavenBlockLocation("world", 1, 2, 3)));
+		assertThat(location, not(equalTo(new HeavenBlockLocation(null, 1, 2, 3))));
+		assertThat(location, not(equalTo(new HeavenBlockLocation("world_nether", 1, 2, 3))));
+		assertThat(location, not(equalTo(new HeavenBlockLocation("world", 10, 2, 3))));
+		assertThat(location, not(equalTo(new HeavenBlockLocation("world", 1, 20, 3))));
+		assertThat(location, not(equalTo(new HeavenBlockLocation("world", 1, 2, 30))));
 	}
 
-	public void testHashCore()
+	public void testHashCode()
 	{
 		final int hashCode = new HeavenBlockLocation("world", 1, 2, 3).hashCode();
 
-		assertEquals(hashCode, new HeavenBlockLocation("world", 1, 2, 3).hashCode());
-		assertNotSame(hashCode, new HeavenBlockLocation("world_nether", 1, 2, 3).hashCode());
-		assertNotSame(hashCode, new HeavenBlockLocation("world", 10, 2, 3).hashCode());
-		assertNotSame(hashCode, new HeavenBlockLocation("world", 1, 20, 3).hashCode());
-		assertNotSame(hashCode, new HeavenBlockLocation("world", 1, 2, 30).hashCode());
+		assertThat(hashCode, equalTo(new HeavenBlockLocation("world", 1, 2, 3).hashCode()));
+		assertThat(hashCode, not(equalTo(new HeavenBlockLocation(null, 1, 2, 3).hashCode())));
+		assertThat(hashCode, not(equalTo(new HeavenBlockLocation("world_nether", 1, 2, 3).hashCode())));
+		assertThat(hashCode, not(equalTo(new HeavenBlockLocation("world", 10, 2, 3).hashCode())));
+		assertThat(hashCode, not(equalTo(new HeavenBlockLocation("world", 1, 20, 3).hashCode())));
+		assertThat(hashCode, not(equalTo(new HeavenBlockLocation("world", 1, 2, 30).hashCode())));
+	}
+
+	public void testToString()
+	{
+		assertThat(new HeavenBlockLocation("world", 1, 2, 3).toString(), equalTo("(world, 1, 2, 3)"));
 	}
 }
