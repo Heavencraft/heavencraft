347090579fc4c0041db59170bafd1a3c2190a0f0
LRGN <lrgn@protonmail.com>
2017-01-01 14:39:03 +0900

    HeavenRP: fix ClassCastException when trying to access horse with chest

--- heavencore-bukkit/src/main/java/fr/hc/core/horses/ChevalCommand.java
+++ heavencore-bukkit/src/main/java/fr/hc/core/horses/ChevalCommand.java
@@ -11,7 +11,6 @@ import fr.hc.core.exceptions.HeavenException;
 import fr.hc.core.utils.PlayerUtil;
 import fr.hc.core.utils.chat.ChatUtil;
 
-@SuppressWarnings("deprecation")
 public class ChevalCommand extends AbstractCommandExecutor
 {
 	public ChevalCommand(BukkitHeavenCore plugin)
