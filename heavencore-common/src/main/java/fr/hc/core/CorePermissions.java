9c185be46bc0427d0d3b952f822a8106572ecf1c
Mythridor <enoubia@gmail.com>
2017-01-06 21:41:05 +0100

    TpworldCommand added and TpCommand fixed

--- heavencore-common/src/main/java/fr/hc/core/CorePermissions.java
+++ heavencore-common/src/main/java/fr/hc/core/CorePermissions.java
@@ -17,6 +17,7 @@ public class CorePermissions
 	public static final String TPHERE_COMMAND = "heavencore.commands.tphere";
 	public static final String TPPOS_COMMAND = "heavencore.commands.tppos";
 	public static final String POOF_COMMAND = "heavencore.commands.poof";
+	public static final String TPWORLD_COMMAND = "heavencore.commands.tpworld";
 
 	public static final String HORSE_BYPASS = "heavencore.horses.bypass";
 }
