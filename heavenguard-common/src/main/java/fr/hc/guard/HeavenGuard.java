ec1ec55fd95553614ad57a883c4e37f4318cc97b
lorgan17 <lorgan17@gmail.com>
2017-02-04 20:33:10 +0900

    HeavenGuard: make world region automatically

--- heavenguard-common/src/main/java/fr/hc/guard/HeavenGuard.java
+++ heavenguard-common/src/main/java/fr/hc/guard/HeavenGuard.java
@@ -3,6 +3,7 @@ package fr.hc.guard;
 import fr.hc.core.connection.ConnectionProvider;
 import fr.hc.core.db.users.User;
 import fr.hc.core.db.users.UserProvider;
+import fr.hc.guard.db.GlobalRegionProvider;
 import fr.hc.guard.db.RegionProvider;
 
 public interface HeavenGuard
@@ -11,9 +12,12 @@ public interface HeavenGuard
 
 	RegionProvider getRegionProvider();
 
+	GlobalRegionProvider getGlobalRegionProvider();
+
 	RegionManager getRegionManager();
 
 	UserProvider<? extends User> getUserProvider();
 
 	void setUserProvider(UserProvider<? extends User> userProvider);
+
 }
